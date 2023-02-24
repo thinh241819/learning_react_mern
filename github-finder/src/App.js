5f0a46b081f575d04a7387ebeae87887123b1d3b
Charles (Ngoc) Nguyen <59405079+thinh241819@users.noreply.github.com>
2023-02-24 07:18:13 +0000

    Complete github_finder section3

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
-  foo = () => {
-    return 'Bar'
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ users: (await res).data, loading: false })
   }
   render () {
-    const name = 'John Doe';
     return (
       <div className="App">
-        <h1>Hello from {this.foo()}</h1>
-        <h1>Hello from {name}</h1>
+        <Navbar title="Github Finder" icon="fab fa-github"></Navbar>
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
---
+++ github-finder/src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = ({ icon, title }) => {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={icon}></i>
+          {title}
+        </h1>
+      </nav>
+    )
+}
+
+export default Navbar
